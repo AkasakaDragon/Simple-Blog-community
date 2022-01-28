3c3ba69dc022f86c7ee59f636f0cf69fb45c385b
tom_glb <goodMorning_glb@atguigu.com>
2022-01-28 13:02:39 +0800

    finally

--- blog-admin/blog-admin/src/main/java/com/mszlu/blog/admin/mapper/PermissionMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mszlu.blog.admin.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mszlu.blog.admin.pojo.Permission;
-
-public interface PermissionMapper extends BaseMapper<Permission> {
-}
